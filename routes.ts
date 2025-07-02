f00687ac0eaf42e29d186b68b0c063c84a334571
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2025-07-02 22:04:37 +0530

    feat(routes): define public, auth, and API auth routes with default login redirect

---
+++ routes.ts
@@ -0,0 +1,33 @@
+/**
+ * An array of routes that are accessible to public
+ * These routes do not need authentication
+ * @type {string[]}
+ */
+export const publicRoutes = [
+    "/",
+]
+
+/**
+ * Authentications routes are used for login,register etc
+ * when a user already authenticated they should redirected away from these.
+ * @type {string[]}
+ */
+
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register"
+]
+
+/**
+ * A prefix for API authentication routes.
+ * NextAuth uses this to handle requests like `/api/auth/*`
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth"
+
+/**
+ * Default redirect path after logged in
+ * @type {string}
+ */
+
+export const default_login_redirect="/settings"
