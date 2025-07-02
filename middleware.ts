8caf59be0b1640be1212e4a808fd72e4baa9e33a
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2025-07-02 22:03:13 +0530

    feat: add NextAuth auth() to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,35 @@
 import authConfig from "./auth.config"
 import NextAuth from "next-auth"
+import { publicRoutes, authRoutes, apiAuthPrefix, default_login_redirect } from "./routes";
+import { NextResponse } from "next/server";
+
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
     console.log("From middleware");
-
     console.log("Route", !req.auth);
 
+    const isApiAuthRoute = req.nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(req.nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(req.nextUrl.pathname);
+
+    // Let NextAuth handle its own API routes like /api/auth/*
+    if (isApiAuthRoute) return NextResponse.next();
+
+    // Allow public routes to everyone 
+    if (isPublicRoute) return NextResponse.next();
+
+    // If the user is already logged in and is trying to access /login -> send them to app
+    if (isAuthRoute && req.auth) {
+        return NextResponse.redirect(new URL(default_login_redirect, req.url));
+    }
+    // If the user is not logged in and tries to access a private page -> send to login
+    if (!req.auth) {
+        return NextResponse.redirect(new URL("/login", req.url))
+    }
+
+    return NextResponse.next();
 })
 
 export const config = {
