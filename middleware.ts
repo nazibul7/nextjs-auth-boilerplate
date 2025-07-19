6902f078571712b3ca9c6ce15a068bbae519b506
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2025-07-19 14:04:51 +0530

    feat(middleware): refine auth route check to allow access to /login

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,23 @@
-import authConfig from "./auth.config"
-import NextAuth from "next-auth"
+import { auth } from "./auth";
 import { publicRoutes, authRoutes, apiAuthPrefix, default_login_redirect } from "./routes";
 import { NextResponse } from "next/server";
 
 
-const { auth } = NextAuth(authConfig);
+/**
+ * This is the Auth.js middleware to protect routes.
+ * 
+ * The auth() helper from Auth.js runs the callback function.
+ * It wraps the function with its internal getSession logic → that logic:
+ * - verifies the JWT,
+ * - runs your jwt callback,
+ * - runs your session callback,
+ * - attaches it to req.auth
+ */
 
 export default auth((req) => {
-    console.log("From middleware");
-    console.log("Route", !req.auth);
+    // console.log("From middleware");
+    // console.log("Route", !!req.auth);
+    // console.log(authRoutes[0], req.url);
 
     const isApiAuthRoute = req.nextUrl.pathname.startsWith(apiAuthPrefix);
     const isPublicRoute = publicRoutes.includes(req.nextUrl.pathname);
@@ -21,12 +30,12 @@ export default auth((req) => {
     if (isPublicRoute) return NextResponse.next();
 
     // If the user is already logged in and is trying to access /login -> send them to app
-    if (isAuthRoute && req.auth) {
+    if (isAuthRoute && !!req.auth) {
         return NextResponse.redirect(new URL(default_login_redirect, req.url));
     }
     // If the user is not logged in and tries to access a private page -> send to login
-    if (!req.auth) {
-        return NextResponse.redirect(new URL("/login", req.url))
+    if (!req.auth && !isAuthRoute) {
+        return NextResponse.redirect(new URL(authRoutes[0], req.url))
     }
 
     return NextResponse.next();
