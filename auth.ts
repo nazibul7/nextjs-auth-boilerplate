35aeb9da7125a1cc13fc1eee8ec93d144ff6367f
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2025-07-25 17:24:36 +0530

    feat(auth): auto-verify OAuth emails in signIn callback & custom auth pages

--- auth.ts
+++ auth.ts
@@ -21,7 +21,34 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     ...authConfig,
     callbacks: {
         /**
-         * ✅ jwt callback:
+         * signIn callback:
+         * - Runs **only** at sign-in.
+         * - Allows you to control if a user can sign in.
+         * - Return `true` to allow sign-in.
+         * - Return `false` or throw an error to deny sign-in.
+         *
+         * Use cases:
+         * - Enforce email verification.
+         * - Restrict sign-ins based on user role, status, or provider.
+         * - Auto-verify OAuth emails.
+         */
+        async signIn({ user, account }) {
+            // For OAuth providers, email is already verified by the provider
+            if (account?.provider != "credentials") {
+                if (user.email && !user.emailVerified) {
+                    await db.user.update({
+                        where: { email: user.email },
+                        data: { emailVerified: new Date() }
+                    });
+                }
+                return true;
+            }
+            // For credentials login, email verification is setup in     authorize()
+            return true;
+        },
+
+        /**
+         * jwt callback:
          * Runs every time a JWT is created/updated/read.
          * - When the user first signs in, `user` is defined.
          * - On future requests, `user` is undefined — only the `token` is reused.
@@ -39,17 +66,28 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         },
 
         /**
-         * ✅ session callback:
+         * session callback:
          * Shapes what `useSession` & `getSession` return to your frontend.
          * - Runs on every request that needs session.
          * - Pulls custom fields from JWT & attaches them to `session.user`.
          * - If you forget to copy, session.user will be empty!
         */
         async session({ session, token }) {
+            // console.trace("Show me");  Who is calling?
             session.user.id = token.id as string;
             session.user.email = token.email as string;
             session.user.role = token.role as UserRole
             return session;
         },
-    }
+    },
+    pages: {
+        /**
+         * Custom pages:
+         * - Override default NextAuth pages for signIn and error.
+         * - Useful when using custom forms and handling UI myself.
+         */
+        signIn: '/auth/login', // Custom login page
+        error: '/auth/error',  // Redirect here on auth errors (e.g., OAuth or callback issues)
+    },
+
 })  
