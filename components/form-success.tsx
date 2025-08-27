9fcfd78f3af59bbe1f04adcd8986b3aacebf52c0
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2025-08-27 17:29:07 +0530

    feat(ui): improve success message component - Updated FormSuccess component styles for better alignment and readability

--- components/form-success.tsx
+++ components/form-success.tsx
@@ -7,9 +7,9 @@ interface FormErrorProps {
 export default function FormSuccess({ message }: FormErrorProps) {
     if (!message) return null;
     return (
-        <div className="bg-emerald-500/15 rounded-md text-sm p-[10px] gap-x-2 text-emerald-500 flex items-center justify-center">
-            <FaCheckCircle className="h-4 w-4" />
-            <p>{message}</p>
+        <div className="bg-emerald-500/15 rounded-md text-sm p-[10px] gap-2 text-emerald-500 flex justify-center text-center">
+            <FaCheckCircle className="h-4 w-4 flex flex-shrink-0 mt-[3px] " />
+            <p className="text-start">{message}</p>
         </div>
     )
 }
